f0a6f2e64066e19c6ffd6270b4d3121304196133
rald <thecoconutnutisagiantnut@gmail.com>
2026-09-01 07:05:57 +0800

    OK

--- stoat/cite.py
+++ stoat/cite.py
@@ -6,6 +6,8 @@ import re
 conn=sqlite3.connect('kjv.db')
 curs=conn.cursor()
 
+
+
 # Example citation strings
 citations = ["John 3:16", "1 Corinthians 13:4-7", "Genesis 1:1"]
 
@@ -20,16 +22,24 @@ for citation in citations:
         data = match.groupdict()
         
         # Clean up extra spaces and convert numbers to integers
-        data["book"] = data["book"].strip()
-        data["chapter"] = int(data["chapter"])
-        data["start_verse"] = int(data["start_verse"])
+        bnam = data["book"].strip()
+        cnum = int(data["chapter"])
+        svnum = int(data["start_verse"])
         
         # Use a ternary operator to handle missing end verses safely
-        data["end_verse"] = int(data["end_verse"]) if data["end_verse"] is not None else None
+        evnum = int(data["end_verse"]) if data["end_verse"] is not None else None
 
-
-
-        print(data)
+        curs.execute(f"""
+            SELECT 
+                v.id, 
+                b.bnam, 
+                v.cnum, 
+                v.vnum, 
+                v.text
+            FROM verses v
+            JOIN books b ON v.bnum = b.bnum
+            WHERE book={bnam} and chapter={cnum} and verse={svnum} <= verse={scnum};
+        """)
 
 conn.commit()
 conn.close()
